a789bb619c44971039db0feb3df567831350f352
Ian Bowers <Ian Bowers>
2016-07-30 16:46:03 -0400

    Update initial files

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//Press start removes the "Start" screen and loads in the quiz
+
+//Countdown timer immediately starts on quizForm load
+
+//Check if answers are correct and log correct, incorrect, and unanswered questions
+
+//Load results page
