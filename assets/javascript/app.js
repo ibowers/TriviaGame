4b3a2c32a41034e952bc081c8b5b881c0be7dbc4
Ian Bowers <Ian Bowers>
2016-07-30 22:08:33 -0400

    Update files

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,24 @@
-//Press start removes the "Start" screen and loads in the quiz
+$(document).ready(function(){
 
-//Countdown timer immediately starts on quizForm load
+//Hide quizForm and quizResults
+$('#quizForm').hide();
+$('#quizResults').hide();
+
+//Press start removes the "Start" screen loads in the quiz and starts timer
+$('#startButton').click(function(){
+	$('#quizStart').hide();
+	$('#quizForm').show();
+	var timer = 30;
+	var interval = setInterval (function () {
+		timer--;
+		$('#triviaTimer').text(timer);
+		if (timer === 0) clearInterval(interval);
+	}, 1000);
+});
 
 //Check if answers are correct and log correct, incorrect, and unanswered questions
 
 //Load results page
+
+
+});
